b8ff26c8de6d3d4c2a35e2a2c8fa7956486ac6db
marcos <marcos.txt@gmail.com>
2017-11-27 23:36:09 -0200

    make it better

--- src/com/contact/jdbc/ConnectionFactory.java
+++ src/com/contact/jdbc/ConnectionFactory.java
@@ -1,31 +1,30 @@
 package com.contact.jdbc;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory{
 
-	String FORNAME_URL	 		= "com.mysql.jdbc.Driver";
-	String DATABASE 			= "PROJECTS";
-	String USERNAME				= "root";
-	String PASSWORD				= "root";
-	
-	String USERNAME_OPENSHIFT 	= System.getenv("OPENSHIFT_MYSQL_DB_USERNAME");
-	String PASSWORD_OPENSHIFT 	= System.getenv("OPENSHIFT_MYSQL_DB_PASSWORD");
-	String DB_NAME_OPENSHIFT 	= System.getenv("OPENSHIFT_APP_NAME");
-    String PORT_OPENSHIFT 		= System.getenv("OPENSHIFT_MYSQL_DB_PORT");
-    String HOST_OPENSHIFT 		= System.getenv("OPENSHIFT_MYSQL_DB_HOST");
-    
-    String URL_OPENSHIFT 		= "jdbc:mysql://" + HOST_OPENSHIFT + ":" + PORT_OPENSHIFT + "/" + DATABASE;
-    String URL					= "jdbc:mysql://localhost/" + DATABASE + "?autoReconnect=true&useSSL=false";
+	private String FORNAME_URL	 		= "com.mysql.jdbc.Driver";
+	private String DATABASE 			= "PROJECTS";
+	private String USERNAME				= "root";
+	private String PASSWORD				= "root";
+	private String URL					= "jdbc:mysql://localhost/" + DATABASE + "?autoReconnect=true&useSSL=false";
 
-    public Connection getConnection(){
+    
+    public Connection getConnection() throws URISyntaxException{
+    	URI dbUri = new URI(System.getenv("DATABASE_URL"));
+    	USERNAME = dbUri.getUserInfo().split(":")[0];
+        PASSWORD = dbUri.getUserInfo().split(":")[1];
+        URL = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+        
 		try{
-			Class.forName(FORNAME_URL);
-			//return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+			//Class.forName(FORNAME_URL);
 			return DriverManager.getConnection(URL, USERNAME, PASSWORD);
-		}catch(SQLException | ClassNotFoundException e){
+		}catch(SQLException e){
 			e.printStackTrace();
 			throw new RuntimeException(e);
 		}
